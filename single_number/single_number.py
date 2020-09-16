9e3b21d01ddf27ad2a98f01182ea271d1a67f845
antonio martinez baez <tono.mtzb@gmail.com>
2020-09-16 13:59:17 -0500

    completed single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,25 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # the array will always be an odd number 
+    # how can I count if I already saw a number
+    # I need someway to loop over each int
 
-    pass
+    # loop over array
+    # add number to new array
+    # if new number is already there pop existing number
+    # otherwise add number 
+    # the resulting array should only have one number
+
+    not_seen = []
+
+    for i in arr:
+        if i in not_seen:
+            not_seen.remove(i)
+        else:
+            not_seen.append(i)
+
+    return not_seen[0]
 
 
 if __name__ == '__main__':
