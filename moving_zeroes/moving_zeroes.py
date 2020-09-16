b93c555ea2c1fc0b17b58e34dff2b8e29ad25f8d
antonio martinez baez <tono.mtzb@gmail.com>
2020-09-16 14:06:02 -0500

    completed moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,20 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # check if number is not 0, if true append it to new array
+    # if they are zero add it to zeros array
+    # count length of non zero array
+    # append both arrays 
+    non_zero = []
+    zero =[]
 
-    pass
+    for i in arr:
+        if i == 0:
+            zero.append(i)
+        else:
+            non_zero.append(i)
+        
+    return non_zero + zero
 
 
 if __name__ == '__main__':
