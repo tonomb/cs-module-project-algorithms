2679b320e0312f24fe8d19fb2e04b3acd54a4599
antonio martinez baez <tono.mtzb@gmail.com>
2020-09-16 14:35:54 -0500

    completed product of all other numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,14 +3,29 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
+    # not multiplying by the index at value is equal to multiplying by 1 
+    # how do you change the value at a index without changing the original array
+    # once the value is changed multiply all numbers 
 
-    pass
+    multiplied = [0 for _ in arr]
+
+    for i in range(len(arr)):
+        copy = arr[:]
+        # copy has a value of 1 at the index, original array not changed
+        copy[i] = 1
+        # multiply all numbers in the copy array
+        result = 1
+        for x in copy:
+            result *= x
+       
+        multiplied[i] = result
+        
+    return multiplied
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
